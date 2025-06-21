da3d7ed36f095e514ab78a097e7863e727d10bc0
Amaranto19 <blasquez1999@gmail.com>
2025-06-20 22:59:40 -0700

    static y urls falta agregar en views para app lista cat

---
+++ iniciar_sesion/urls.py
@@ -0,0 +1,11 @@
+# mi_app/urls.py
+
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name=''),  # ejemplo de ruta
+]
+
+
+# todo agregar el url y las views
