31e76a35ce4430ddba5cae08121eb0b07d15e508
Amaranto <oscarpersonalsispimx@gmail.com>
2025-06-24 12:41:40 -0700

    modificado carpeta iniciar_sesion

--- iniciar_sesion/urls.py
+++ iniciar_sesion/urls.py
@@ -1,10 +1,7 @@
-
 from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name=''),  # ejemplo de ruta
+    path('', views.iniciar_sesion_view, name='iniciar_sesion'),
+    path('cerrar/', views.cerrar_sesion_view, name='cerrar_sesion'),
 ]
-
-
-# todo agregar el url y las views
--- iniciar_sesion/views.py
+++ iniciar_sesion/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 # Create your views here.
+def iniciar_sesion_view(request):
+    if request.method == 'POST':
+        usuario = request.POST['username']
+        clave = request.POST['password']
+        user = authenticate(request, username=usuario, password=clave)
+        if user is not None:
+            login(request, user)
+            return redirect('home')  # vista en lista cat(app)
+        else:
+            messages.error(request, 'Usuario o contraseña incorrectos.')
+    return render(request, 'login.html')
+
+def cerrar_sesion_view(request):
+    logout(request)
+    return redirect('iniciar_sesion')
